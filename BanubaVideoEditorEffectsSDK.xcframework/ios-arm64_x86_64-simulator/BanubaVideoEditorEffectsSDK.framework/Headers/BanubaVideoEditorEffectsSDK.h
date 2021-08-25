a270470cbd9c87464341c5fb052bd614c6043614
Gleb Markin <gleb.markin@banuba.com>
2021-08-25 15:31:33 +0300

    0.2.independent.beta

---
+++ BanubaVideoEditorEffectsSDK.xcframework/ios-arm64_x86_64-simulator/BanubaVideoEditorEffectsSDK.framework/Headers/BanubaVideoEditorEffectsSDK.h
@@ -0,0 +1,19 @@
+//
+//  BanubaVideoEditorEffectsSDK.h
+//  BanubaVideoEditorEffectsSDK
+//
+//  Created by Gleb Markin on 25.08.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BanubaVideoEditorEffectsSDK.
+FOUNDATION_EXPORT double BanubaVideoEditorEffectsSDKVersionNumber;
+
+//! Project version string for BanubaVideoEditorEffectsSDK.
+FOUNDATION_EXPORT const unsigned char BanubaVideoEditorEffectsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BanubaVideoEditorEffectsSDK/PublicHeader.h>
+
+#import <BanubaVideoEditorEffectsSDK/BNBValuesAnimator.h>
+#import <BanubaVideoEditorEffectsSDK/AssetHelper.h>
