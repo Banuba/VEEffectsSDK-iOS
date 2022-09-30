ea534ebe4456f6148a95b8bb650f9b5f00c49369
sdk-banuba <71497187+sdk-banuba@users.noreply.github.com>
2022-10-01 01:38:29 +0300

    1.25.0

--- VEEffectsSDK.xcframework/ios-arm64_x86_64-simulator/VEEffectsSDK.framework/Headers/AssetHelper.h
+++ VEEffectsSDK.xcframework/ios-arm64_x86_64-simulator/VEEffectsSDK.framework/Headers/AssetHelper.h
@@ -9,7 +9,6 @@
 #import <Foundation/Foundation.h>
 
 size_t rearrange_lut3d_pixels( uint8_t const * pixels2d, size_t w, size_t h, uint8_t * pixels3d );
-size_t rearrange_lut3d_pixels_for_metal( uint8_t const * pixels2d, size_t w, size_t h, uint8_t * pixels3d );
 unsigned char* loadPng(char const *filename, int *x, int *y, int *comp, int req_comp);
 void freePng(unsigned char *image);
 
